f36068accd8ff129d4dad953412bbea78493cdb9
Alex Freeman <a.j.freeman2@newcastle.ac.uk>
2024-04-08 12:35:34 +0100

    created meal csv files and created results page to output them along with routes to get to them based on choices in the views file

--- NutritionQuestions/views.py
+++ NutritionQuestions/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, Blueprint, redirect, url_for, request
-from NutritionQuestions.forms import QuestionOne, QuestionTwo, QuestionThree, QuestionFour, QuestionFive, QuestionSix, QuestionSeven
+from NutritionQuestions.forms import QuestionOne, QuestionTwo, QuestionThree
+from FitnessQuestions.views import read_csv
 
 meal_blueprint = Blueprint('meal', __name__, template_folder='templates/features/nutritionQs')
 
@@ -38,7 +39,52 @@ def meal_result():
     if len(choices) != 2:
         return redirect("/meal")
     if choices[0] <= "3":
-        pass
+        water = "Please ensure you are drinking at least 3 litres of water per day"
     else:
-        pass
-    return render_template("features/nutritionQs/result.html")
+        water= "Well done, ensure you continue drinking 3 or more litres of water per day"
+    if len(choices[1]) >=3:
+        meal, recipe = read_csv('static/csv/meals/all.csv')
+        return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+    if (choices[1][0]) == "none":
+        meal, recipe = read_csv('static/csv/meals/none.csv')
+        return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+    if 'gluten' in choices[1] or 'keto' in choices[1]:
+        if 'nut' in choices[1]:
+            meal, recipe = read_csv('static/csv/meals/n_gk.csv')
+            return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+        elif 'vegan' in choices[1] or 'lactose' in choices[1]:
+            meal, recipe = read_csv('static/csv/meals/vl_gk.csv')
+            return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+        elif 'veg' in choices[1]:
+            meal, recipe = read_csv('static/csv/meals/gk_veg.csv')
+            return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+        elif 'pesc' in choices[1]:
+            meal, recipe = read_csv('static/csv/meals/gk_p.csv')
+            return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+        meal, recipe = read_csv('static/csv/meals/gk.csv')
+        return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+    elif 'vegan' in choices[1] or 'lactose' in choices[1]:
+        if 'nut' in choices[1]:
+            meal, recipe = read_csv('static/csv/meals/n_vl.csv')
+            return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+
+        meal, recipe = read_csv('static/csv/meals/vl.csv')
+        return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+
+    elif 'veg' in choices[1]:
+        if 'nut' in choices[1]:
+            meal, recipe = read_csv('static/csv/meals/n_veg.csv')
+            return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+
+        meal, recipe = read_csv('static/csv/meals/veg.csv')
+        return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+    elif 'nut' in choices[1]:
+        if 'pesc' in choices[1]:
+            meal, recipe = read_csv('static/csv/meals/n_p.csv')
+            return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+        meal, recipe = read_csv('static/csv/meals/nut.csv')
+        return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
+
+    elif 'pesc' in choices[1]:
+        meal, recipe = read_csv('static/csv/meals/pesc.csv')
+        return render_template("features/nutritionQs/result.html", meal=meal, recipe=recipe, water=water)
